980004c47bae9507aa1d1f32a3f9d7c789c5ae3f
firdausArdiansyah <muhammadfirdausardiansyah77@gmail.com>
2025-12-16 15:32:39 +0700

    Add files via upload

---
+++ Muhammad Firdaus Ardiansyah/soal2/main.go
@@ -0,0 +1,26 @@
+package main 
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var n float64
+	fmt.Print("Masukkan n: ")
+	fmt.Scan(&n)
+
+	batas := math.Ceil(n)
+
+	saatIni := int(n * 10)
+	nLanjutan := int(batas * 10)
+	
+
+	for i := saatIni + 1; i <= nLanjutan; i++{
+		if i%10 == 0 {
+			fmt.Println(i / 10)
+		} else {
+			fmt.Printf("%.1f\n", float64(i) / 10.0)
+		}
+	}
+
+}
